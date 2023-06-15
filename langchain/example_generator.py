8dd3eed349143ab177c94e1c8546e1ec0e3dd7f3
terryabl <136738664+terryabl@users.noreply.github.com>
2023-06-16 04:11:51 +0800

    Update example_generator.py

--- langchain/example_generator.py
+++ langchain/example_generator.py
@@ -1,14 +1,6 @@
 """Utility functions for working with prompts."""
 from typing import List
 
-from langchain.base_language import BaseLanguageModel
-from langchain.chains.llm import LLMChain
-from langchain.prompts.few_shot import FewShotPromptTemplate
-from langchain.prompts.prompt import PromptTemplate
-
-TEST_GEN_TEMPLATE_SUFFIX = "Add another example."
-
-
 def generate_example(
     examples: List[dict], llm: BaseLanguageModel, prompt_template: PromptTemplate
 ) -> str:
